f290e7be67c1b4e53f16f9d9a520e0804313d4f9
Lucas Moran <lucas.moran@snhu.edu>
2013-04-30 16:28:58 -0400

    Iteration 1.1

--- src/main/java/com/goldrushmc/bukkit/train/Train.java
+++ src/main/java/com/goldrushmc/bukkit/train/Train.java
@@ -14,76 +14,63 @@ public class Train implements ITrain {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	public void start() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public List<Player> getPassengers() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<ItemStack> getPayload() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void setPassengers(List<Player> passengers) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void setPayload(List<ItemStack> payload) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public String nextStop() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Set<String> getStops() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void addStop(String stopName, Location stopLoc) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void removeStop(String stopName) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public boolean isBlocked() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public void setBlocked(boolean isBlocked) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
