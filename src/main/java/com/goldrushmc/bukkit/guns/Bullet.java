8e78f65f155eb23964934e37d0bda2fcd0d8aa7e
Lex Webb <lexwebb@gmail.com>
2013-05-14 22:33:58 +0100

    Added findCorners to train

--- src/main/java/com/goldrushmc/bukkit/guns/Bullet.java
+++ src/main/java/com/goldrushmc/bukkit/guns/Bullet.java
@@ -1,8 +1,22 @@
 package com.goldrushmc.bukkit.guns;
 
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Snowball;
+import org.bukkit.craftbukkit.v1_5_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_5_R3.entity.CraftProjectile;
+import org.bukkit.entity.EntityType;
+import com.goldrushmc.bukkit.guns.EntityBullet;
 
-public interface Bullet extends Projectile{
+public class Bullet extends CraftProjectile{
 
+	public Bullet(CraftServer server, EntityBullet entity) {
+		super(server, entity);
+	}
+
+	@Override
+	public EntityType getType() {
+		return EntityType.SNOWBALL;
+	}
+
+	public EntityBullet getHandle() {
+		return (EntityBullet) entity;
+	}
 }
