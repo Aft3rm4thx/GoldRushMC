13ab0cf48cfe6b02f55334718c6bbac0da2ab4f1
Lex Webb <lexwebb@gmail.com>
2013-05-14 21:09:39 +0100

    More Gun Stuff

--- src/main/java/com/goldrushmc/bukkit/guns/Bullet.java
+++ src/main/java/com/goldrushmc/bukkit/guns/Bullet.java
@@ -1,8 +1,8 @@
 package com.goldrushmc.bukkit.guns;
 
 import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
 
-public interface Bullet extends Projectile {
+public interface Bullet extends Projectile{
 
-	
 }
